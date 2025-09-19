135833fe0fa3820167621290e0ad65f1a3773ab8
20231TIIMI0260 <luizhenrique.oliveiradahora@gmail.com>
2025-09-19 09:03:39 -0300

    87

--- app/src/main/java/com/luiz/task/ui/HomeFragment.kt
+++ app/src/main/java/com/luiz/task/ui/HomeFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.luiz.task.R
 import com.luiz.task.databinding.FragmentDoingBinding
 import com.luiz.task.databinding.FragmentHomeBinding
+import com.luiz.task.ui.adapter.ViewPagerAdapter
 
 class HomeFragment : Fragment() {
 
@@ -21,6 +23,25 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initTabs()
+    }
+    private fun initTabs() {
+        val pageAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pageAdapter
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_doing)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_done)
+
+        binding.viewPager.offscreenPageLimit = pageAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager){tab, position ->
+            tab.text = getString(pageAdapter.getTitle(position))
+        }.attach()
     }
 
     override fun onDestroyView() {
